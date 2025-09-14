d8f406e2586fee9452271f5c5d8e1389859ddb5d
Maksim Starostenko <maksim.starostenko.92@gmail.com>
2025-09-14 21:43:50 +0300

    Addel Lesson_1 Lesson_2 Lesson_2_conspect

--- Lesson_1.py
+++ Lesson_1.py
@@ -1,8 +1,9 @@
 import time
-from webbrowser import Chrome
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.common.by import By
+
 
 BROWSER = 'chrome' # поменяй на "chrome" чтобы запустить Chrome
 
@@ -10,7 +11,10 @@ if BROWSER == 'chrome': # запускаем Chrome
     chrome_options = ChromeOptions()
     # оставить окно открытым после завершения скрипта (удобно при обучении)
     chrome_options.add_experimental_option('detach', True)
+    # 🔑 запуск в гостевом режиме
+    chrome_options.add_argument('--guest')
     driver = webdriver.Chrome(options=chrome_options)
+
 
 elif BROWSER == 'firefox':
     firefox_options = FirefoxOptions()
@@ -22,8 +26,18 @@ else:
 
 driver.get('https://www.saucedemo.com/')
 driver.maximize_window()
-time.sleep(30) # чтобы увидеть результат
-driver.quit()  # при detach=True у Chrome можно вызвать driver.quit() позже вручную
+
+user_name = driver.find_element(By.ID, "user-name")
+user_name.send_keys("standard_user")
+
+user_name = driver.find_element(By.ID, "password")
+user_name.send_keys("secret_sauce")
+
+user_name = driver.find_element(By.ID, "login-button")
+user_name.click()
+
+# time.sleep(30) # чтобы увидеть результат
+#driver.quit()  # при detach=True у Chrome можно вызвать driver.quit() позже вручную
 
 
 
