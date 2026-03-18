47922cccffe52a456bf1cef76a1c7f6ee88f81e8
Maksim Starostenko <maksim.starostenko.92@gmail.com>
2026-03-18 17:11:22 +0200

    added test_mail_1, test_mail_2

--- tests_mail/test_mail_1.py
+++ tests_mail/test_mail_1.py
@@ -4,7 +4,6 @@ import pytest
 def set_up():
     print("Вход в систему выполнен")
 def test_sending_mail_1(set_up):
-    print("Почта отправлена")
+    print("Почта 1 отправлена")
 def test_sending_mail_2(set_up):
-    print("Почта отправлена")
-
+    print("Почта 2 отправлена")
--- tests_mail/test_mail_2.py
+++ tests_mail/test_mail_2.py
@@ -4,6 +4,7 @@ import pytest
 def set_up():
     print("Вход в систему выполнен")
 def test_sending_mail_3(set_up):
-    print("Почта отправлена")
+    print("Почта 3 отправлена")
 def test_sending_mail_4(set_up):
-    print("Почта отправлена")
+    print("Почта 4 отправлена")
+
