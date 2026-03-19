c3ffed50bdeb9a5638a0d2748db8540f1aefdc18
Maksim Starostenko <maksim.starostenko.92@gmail.com>
2026-03-19 18:10:08 +0200

    Added yield in test_mail_1 and test_mail_2

--- tests_mail/test_mail_1.py
+++ tests_mail/test_mail_1.py
@@ -3,6 +3,9 @@ import pytest
 @pytest.fixture()
 def set_up():
     print("Вход в систему выполнен")
+    yield
+    print("Выход выполнен")
+
 def test_sending_mail_1(set_up):
     print("Почта 1 отправлена")
 def test_sending_mail_2(set_up):
--- tests_mail/test_mail_2.py
+++ tests_mail/test_mail_2.py
@@ -3,6 +3,10 @@ import pytest
 @pytest.fixture()
 def set_up():
     print("Вход в систему выполнен")
+    yield # условный оператор означает, что все что выше исполняется до теста,
+    # все что ниже yield исполняется после теста
+    print("Выход из системы")
+
 def test_sending_mail_3(set_up):
     print("Почта 3 отправлена")
 def test_sending_mail_4(set_up):
