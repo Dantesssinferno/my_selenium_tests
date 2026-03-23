cb17860e7d9c39b4afbfa2adaf88b6db2e13df25
Maksim Starostenko <maksim.starostenko.92@gmail.com>
2026-03-23 17:57:39 +0200

    Added scope in fixtures

--- tests_mail/test_mail_1.py
+++ tests_mail/test_mail_1.py
@@ -1,12 +1,6 @@
 import pytest
 
-@pytest.fixture()
-def set_up():
-    print("Вход в систему выполнен")
-    yield
-    print("Выход выполнен")
-
-def test_sending_mail_1(set_up):
+def test_sending_mail_1(set_up, some):
     print("Почта 1 отправлена")
-def test_sending_mail_2(set_up):
+def test_sending_mail_2(set_up, some):
     print("Почта 2 отправлена")
--- tests_mail/test_mail_2.py
+++ tests_mail/test_mail_2.py
@@ -1,13 +1,6 @@
 import pytest
 
-@pytest.fixture()
-def set_up():
-    print("Вход в систему выполнен")
-    yield # условный оператор означает, что все что выше исполняется до теста,
-    # все что ниже yield исполняется после теста
-    print("Выход из системы")
-
-def test_sending_mail_3(set_up):
+def test_sending_mail_3(set_up, next):
     print("Почта 3 отправлена")
-def test_sending_mail_4(set_up):
+def test_sending_mail_4(set_up, next):
     print("Почта 4 отправлена")
